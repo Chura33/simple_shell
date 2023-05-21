fc3bcd92caf8f10f831bd2e3cd343dfd461ea19e
Your Name <you@example.com>
2023-05-21 12:54:26 -0700

    merge with partners last push

--- test.c
+++
@@ -1,9 +0,0 @@
-#include "phics.h"
-int main ()
-{
-	char *name = "phics";
-	name = "chura";
-	char arr[] = "anything, daniel i don\'t";
-	arr[0] = 'y';
-	printf("name is %s/narray is %s\n", name, arr);
-}
