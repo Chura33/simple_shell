b65b9e05afc018c66b21c57f188e592cb55e2a9e
oladipo <phaymos2022@gmail.com>
2023-05-24 10:33:22 +0100

    simple_shell

--- simple.c
+++ simple.c
@@ -39,12 +39,6 @@ int main(__attribute__((unused)) int argc, char **argv, char **envp)
 			args[i] = strtok(NULL, delim);
 		}
 		args[i] = NULL;
-		if (i > 1)
-		{
-			perror(argv[0]);
-			i = 0;
-			continue;
-		}
 		i = 0;
 		cmdresult = handle_cmd(args, argv[0], cmd);
 		if (cmdresult == 1)
